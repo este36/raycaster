6d5522060ca15e344828a40f9410913a14bd333c
este36 <mercierstban@gmail.com>
2026-03-06 17:05:02 +0100

    add minimap grid

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,26 @@
 import './style.css'
 
-const _factor = 1.2;
+const _factor = 2;
 const WIDTH = 400 * _factor;
 const HEIGHT = 300 * _factor;
 let fps_printer: HTMLParagraphElement | null = null;
 let frame_count = 0;
 
+// const map = [
+//   1,1,1,1,1,1,1,1,1,1,
+//   1,0,0,0,0,0,0,0,0,1,
+//   1,0,0,0,0,0,0,0,0,1,
+//   1,0,0,0,0,0,0,0,0,1,
+//   1,0,0,0,0,0,0,0,0,1,
+//   1,0,0,0,0,0,0,0,0,1,
+//   1,0,0,0,0,0,0,0,0,1,
+//   1,0,0,0,0,0,0,0,0,1,
+//   1,0,0,0,0,0,0,0,0,1,
+//   1,1,1,1,1,1,1,1,1,1,
+// ];
 
-// const GRID_COLS = 10;
-// const GRID_ROWS = 10;
+const GRID_COLS = 10;
+const GRID_ROWS = 10;
 
 type Renderer = {
   ctx: CanvasRenderingContext2D;
@@ -26,6 +38,48 @@ function putPixel(data: Uint8ClampedArray, x: number, y: number, r: number, g: n
   data[index + 3] = 0xff;
 }
 
+function renderMinimap(r: Renderer): void
+{
+  const { data } = r;
+
+  const minimapWidth = Math.floor(WIDTH / 4);
+  const minimapHeight = Math.floor(HEIGHT / 4);
+  const cellWidth = minimapWidth / GRID_COLS;
+  const cellHeight = minimapHeight / GRID_ROWS;
+
+  const startX = WIDTH - minimapWidth - 5;
+  const startY = HEIGHT - minimapHeight - 5;
+
+  let y = startY;
+  let y_dist = 0;
+
+  while (y_dist < minimapHeight)
+  {
+      let x_dist = 0;
+      let x = startX;
+
+      y_dist = Math.abs(startY - y);
+      while (x_dist < minimapWidth)
+      {
+        x_dist = Math.abs(startX - x);
+        if (x_dist % cellWidth < 0.1)
+        {
+          putPixel(data, x, y, 0x99, 0x99, 0x99);
+        }
+        else if (y_dist % cellHeight < 0.1)
+        {
+          putPixel(data, x, y, 0x99, 0x99, 0x99);
+        }
+        else
+        {
+          putPixel(data, x, y, 0x18, 0x18, 0x18);
+        }
+        x++;
+      }
+      y++;
+  }
+}
+
 function render(r: Renderer): void
 {
   const {ctx, img, data} = r;
@@ -35,12 +89,14 @@ function render(r: Renderer): void
   {
     for (let x = 0; x < WIDTH; x++)
     {
-      const r = ((x * Math.log(frame_count)) % 0xff);
+      const r = (Math.sqrt(x*x + y*y) + frame_count) % 256;
       const g = ((y * Math.log(frame_count)) % 256);
       const b = 150;
       putPixel(data, x, y, r, g, b);
     }
   }
+
+  renderMinimap(r);
 
   ctx.putImageData(img, 0, 0);
   const elapsed = performance.now() - perf1;
